e1fc02772547a479e979106772d2d99b610f04bc
lucumt <lucumt@gmai.com>
2023-10-07 11:41:49 +0800

    update plugin

--- scripts/plugin.js
+++ scripts/plugin.js
@@ -1,15 +1,16 @@
 require(["gitbook", "jquery"], function(gitbook, $) {
-  gitbook.events.bind("page.change", function() {
+  gitbook.events.bind("start", function(e,config) {
     $('code.lang-flow').each(function(index, element) {
       var $element = $(element),
           code = $element.text(),
           chart;
+	  var options = config["flowchart"];	  
 
       var wrapper = $("<div id='canvas"+index+"'></div>");
       $element.parent().replaceWith(wrapper);
 
       chart = flowchart.parse(code);
-      chart.drawSVG('canvas'+index)
+      chart.drawSVG('canvas'+index, options)
     });
   });
 });
